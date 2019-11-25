c621257fca7796af76845ad37718d83b32047ce1
Alice Duff <duff.alice@gmail.com>
2019-11-25 04:53:34 +0000

    Binary Tree Sort

---
+++ README.md
@@ -0,0 +1,3 @@
+## Binary Tree Traversal
+
+Coding challenge
---
+++ lib/BinaryTree.rb
@@ -0,0 +1,65 @@
+class BinaryTree
+  attr_accessor :payload, :left, :right
+
+  def initialize(payload, left, right)
+    @payload = payload
+    @left = nil
+    @right = nil
+  end
+end
+
+class BTreeSort
+
+  def self.sort(array)
+    @start = BinaryTree.new(array.first, nil, nil)
+    @sortarry = []
+    array.each do |x|
+      @look = @start 
+      insert(x)
+    end
+    sortme(@start)
+    return @sortarry
+  end
+
+  def self.insert(value)
+      node = value <=> @look.payload
+      if node != 0 then
+        if node == -1
+          # left
+          if @look.left == nil then
+            @look.left = BinaryTree.new(value, nil, nil)
+          else
+            @look = @look.left
+            self.insert(value)
+          end
+        else
+          # right
+          if @look.right == nil then
+            @look.right = BinaryTree.new(value, nil, nil)
+          else
+            @look = @look.right
+            self.insert(value)
+          end
+        end
+      end
+  end
+
+  def self.sortme(starter)
+    if starter == nil
+      return
+    end
+
+    if starter.left
+      sortme(starter.left)
+    end
+
+    @sortarry << starter.payload
+
+    if starter.right
+      sortme(starter.right)
+    end
+
+  end
+end
+
+BTreeSort.sort([7, 4, 9, 1, 6, 14, 10]).inspect
